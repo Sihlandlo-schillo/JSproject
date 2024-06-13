737069aec45ef55ab78303319cf8ca23a3df0397
sihlandlo-schillo <sihlandlom@gmail.com>
2024-06-13 17:55:05 +0200

    contacts

--- script/cart.js
+++ script/cart.js
@@ -1,16 +1,19 @@
 let name = localStorage.getItem('purchasedItems')
 let main = document.querySelector('tbody')
 let purchasedItems = JSON.parse(localStorage.getItem('purchasedItems'))
+
+let cartInRowTotall = document.getElementById('')
+
 function displayCart(){
     purchasedItems.forEach(purchasedItems =>{
     main.innerHTML += `
                 <tr>
                     <td>${purchasedItems.name}</td>
-                    <td><img src=${purchasedItems.image} class="cartProds"></td>
-                    <td><p>R ${purchasedItems.price}</p></td>
                     <td>${purchasedItems.category}</td>
+                    <td><p>R ${purchasedItems.price}</p></td>
                     <td><select><option  value="number">1</option><option  value="number">2</option><option  value="number">3</option><option  value="number">4</option><option  value="number">5</option><option  value="number">6</option><option  value="number">7</option><option  value="number">8</option><option  value="number">9</option><option  value="number">10</option><option  value="number">11</option><option  value="number">12</option><option  value="number">13</option><option  value="number">14</option><option  value="number">15</option><option  value="number">16</option><option  value="number">17</option><option  value="number">18</option></select></td>
                     <td></td>
+                    <td><button>X</button></td>
                 </tr>
                       `
 })
@@ -21,4 +24,4 @@ let totalButton = document.getElementsByTagName('button')[0]
 let span = document.getElementsByTagName('span')[0]
 let totalTag = document.querySelector('[total]')
 
-// totalTag.innerText = purchasedItems.reduce()
+totalTag.innerText = purchasedItems.reduce()
--- script/products.js
+++ script/products.js
@@ -10,7 +10,7 @@ function CreateItem(id, name, category, image,quantity, description, price) {
 
 // If you want to use it somewhere else you store it in local storage
 
-let item1 = new CreateItem(1, 'FormalWear Bottom', 'Casual','https://sihlandlo-schillo.github.io/HOSTEDIMAGES/images/Yolanda.jpg','', 'Electric Guitar 39 Inch Set - Black', 2999 )
+let item1 = new CreateItem(1, 'Baggy Multi-Pocket Jean', 'Casual','https://sihlandlo-schillo.github.io/HOSTEDIMAGES/images/Yolanda.jpg','', 'Electric Guitar 39 Inch Set - Black', 2999 )
 
 let item2 = new CreateItem(2,'Modern Blazer', 'Formal Wear', 'https://raw.githubusercontent.com/Sihlandlo-schillo/HOSTEDIMAGES/main/images/Blazer.webp', '', 'Welcome to the future of homemade bread! This Stainless Steel Bread Maker, boasting 550W power, is your ticket to effortless baking bliss. With features like a 13-hour delay start and a 1-hour quick bake, enjoy fresh loaves tailored to your schedule. From customisable crusts to versatile settings, elevate your baking experience and savour the taste of perfection!', 1999)
 
@@ -31,19 +31,39 @@ let main = document.querySelector('main')
 function myProds() { 
     items.forEach(item =>{
         main.innerHTML += `
-                        <div class="">
-                            <img src="${item.image}" class="products">
-                            <p class="prices">R${item.price}</p>
-                            <button id="View-more">View More</button>
-                            <button class="purchase" value="${item.id}">Purchase</button>
-                        </div>
-                            `
+        <div class="card" style="width: 18rem;">
+        <img src="${item.image}" class="card-img-top" alt="..." id="itemImage">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text">R${item.price}</p>
+          <button type="button" class="btn btn-secondary" id="view-More" data-bs-toggle="modal" data-bs-target="#exampleModal">View More</button>
+          <button type="button" class="btn btn-secondary" id="purchase" value="${item.id}">Add to Cart</button>
+      </div>
+      <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Purchase</button>
+      </div>
+    </div>
+  </div>
+</div>
+        `
      
     })
     
 }myProds()
 
-let purchaseButtons = document.querySelectorAll('.purchase')
+let purchaseButtons = document.querySelectorAll('#purchase')
  function addToCart(id) {
     // can use a method or a loop
     // items is an array
@@ -76,28 +96,55 @@ function viewing() {
 
  //SORT BUTTON
 
- let select = document.querySelector('#what')
- select.addEventListener('change',()=>{
-     let value = select.value
-     if(value == 'no-filter'){
-         myProds(items)
-         return
-        }
-        console.log(value);
-        let sort = items.filter(item=>{
-            return item.category.includes(select.value)
-        })
-        myProds(sort)
-    })
+//
+let filtering = document.getElementById('what')
+filtering.addEventListener('change', function() {
+    let actualVal = this.value;
+    currentFilter = actualVal; // Store the current filter
+    // Clear the current displayed items
+    main.innerHTML = '';
+    if (actualVal === '') {
+        // If the selected value is an empty string, display all items
+        items.forEach((item, index) => {
+            myProds(item, index);
+        });
+    } else {
+        // Filter the items array
+        let filteredProducts = items.filter(function(item) {
+            return item.category === actualVal;
+        });
+        // Loop through the filtered products array and add each product to the page
+        filteredProducts.forEach((item, index) => {
+            myProds(item, index);
+        });
+    }
+});
 
-    let searchButton = document.getElementById('search')
-    searchButton.addEventListener("click", searchItem)
 
-function searchItem() {
-  let filteredWhippy = items.filter(item => item.name.toLowerCase().includes(input.value.toLowerCase()));
-    main.innerHTML = ''
-    displayItems(filteredWhippy)
-}
+
+
+//  let select = document.querySelector('#what')
+//  select.addEventListener('change',()=>{
+//      let value = select.value
+//      if(value == 'no-filter'){
+//          myProds(items)
+//          return
+//         }
+//         console.log(value);
+//         let sort = items.filter(item=>{
+//             return item.category.includes(select.value)
+//         })
+//         myProds(sort)
+//     })
+
+//     let searchButton = document.getElementById('search')
+//     searchButton.addEventListener("click", searchItem)
+
+// function searchItem() {
+//   let filteredWhippy = items.filter(item => item.name.toLowerCase().includes(input.value.toLowerCase()));
+//     main.innerHTML = ''
+//     displayItems(filteredWhippy)
+// }
 
 
 //  let sortButton = document.getElementById('sort')
