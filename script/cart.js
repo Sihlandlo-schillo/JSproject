cd8719ccb93563e5aabbf2c49f18f5e84b5102bb
sihlandlo-schillo <sihlandlom@gmail.com>
2024-06-14 14:58:51 +0200

    yeah nhe

--- script/cart.js
+++ script/cart.js
@@ -1,18 +1,37 @@
+const footer = document.createElement('footer');
+
+
+footer.innerHTML = 'FASHIONSCENE ©2024 | All Rights Reserved';
+
+footer.style.position = 'fixed';
+footer.style.bottom = '0';
+footer.style.width = '100%';
+footer.style.backgroundColor = '#333';
+footer.style.color = '#fff';
+footer.style.textAlign = 'center';
+footer.style.padding = '10px 0';
+
+footer.classList.add('footer');
+
+document.body.appendChild(footer);
+
+
 let name = localStorage.getItem('purchasedItems')
 let main = document.querySelector('tbody')
 let purchasedItems = JSON.parse(localStorage.getItem('purchasedItems'))
 
-let cartInRowTotall = document.getElementById('')
+let cartInRowTotall = document.getElementById('rowTotal')
 
 function displayCart(){
     purchasedItems.forEach(purchasedItems =>{
+    
     main.innerHTML += `
                 <tr>
                     <td>${purchasedItems.name}</td>
                     <td>${purchasedItems.category}</td>
                     <td><p>R ${purchasedItems.price}</p></td>
-                    <td><select><option  value="number">1</option><option  value="number">2</option><option  value="number">3</option><option  value="number">4</option><option  value="number">5</option><option  value="number">6</option><option  value="number">7</option><option  value="number">8</option><option  value="number">9</option><option  value="number">10</option><option  value="number">11</option><option  value="number">12</option><option  value="number">13</option><option  value="number">14</option><option  value="number">15</option><option  value="number">16</option><option  value="number">17</option><option  value="number">18</option></select></td>
-                    <td></td>
+                    <td><button data-id=${purchasedItems.id} class="quantity subtraction">-</button>${purchasedItems.quantity} <button class="quantity addition" data-id=${purchasedItems.id} >+</button></td>
+                    <td id="rowTotal"></td>
                     <td><button>X</button></td>
                 </tr>
                       `
@@ -20,8 +39,56 @@ function displayCart(){
 }
 displayCart()
 
-let totalButton = document.getElementsByTagName('button')[0]
+let totalButton = document.getElementById('total')
 let span = document.getElementsByTagName('span')[0]
+let subtraction = document.querySelectorAll('.subtraction')
+let addition = document.querySelectorAll('.addition')
+// totalButton.addEventListener('click', )
+function totalCost() {
+    
+}
 let totalTag = document.querySelector('[total]')
 
-totalTag.innerText = purchasedItems.reduce()
+// totalTag.innerText = purchasedItems.reduce()
+
+function increment(id) {
+    let item = purchasedItems.find(item => item.id === +id)
+    if (item) {
+        addition = item.quantity += 1
+        localStorage.setItem('purchasedItems', JSON.stringify(purchasedItems))
+        console.log(addition);
+        displayCart()
+    }
+}
+
+function decrement(id) {
+    let item = purchasedItems.find(item => item.id === +id)
+    if (item && item.quantity > 1) {
+        subtraction = item.quantity -= 1
+        localStorage.setItem('purchasedItems', JSON.stringify(purchasedItems))
+        console.log(subtraction);
+        displayCart()
+    }
+}
+
+function remove() {
+
+}
+function subTotal() {
+
+}
+function overAllOverTotal() {
+
+}
+
+addition.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        increment(event.target.dataset.id)
+    })
+})
+
+subtraction.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        decrement(event.target.dataset.id)
+    })
+})
--- script/products.js
+++ script/products.js
@@ -1,4 +1,4 @@
-function CreateItem(id, name, category, image,quantity, description, price) {
+function CreateItem(id, name, category, image, quantity, description, price) {
     this.id=id
     this.name=name
     this.category=category
@@ -10,17 +10,17 @@ function CreateItem(id, name, category, image,quantity, description, price) {
 
 // If you want to use it somewhere else you store it in local storage
 
-let item1 = new CreateItem(1, 'Baggy Multi-Pocket Jean', 'Casual','https://sihlandlo-schillo.github.io/HOSTEDIMAGES/images/Yolanda.jpg','', 'Electric Guitar 39 Inch Set - Black', 2999 )
+let item1 = new CreateItem(1, 'Baggy Multi-Pocket Jean', 'Casual','https://sihlandlo-schillo.github.io/HOSTEDIMAGES/images/Yolanda.jpg',1, 'Electric Guitar 39 Inch Set - Black', 2999 )
 
-let item2 = new CreateItem(2,'Modern Blazer', 'Formal Wear', 'https://raw.githubusercontent.com/Sihlandlo-schillo/HOSTEDIMAGES/main/images/Blazer.webp', '', 'Welcome to the future of homemade bread! This Stainless Steel Bread Maker, boasting 550W power, is your ticket to effortless baking bliss. With features like a 13-hour delay start and a 1-hour quick bake, enjoy fresh loaves tailored to your schedule. From customisable crusts to versatile settings, elevate your baking experience and savour the taste of perfection!', 1999)
+let item2 = new CreateItem(2,'Modern Blazer', 'Formal Wear', 'https://raw.githubusercontent.com/Sihlandlo-schillo/HOSTEDIMAGES/main/images/Blazer.webp', 1, 'Welcome to the future of homemade bread! This Stainless Steel Bread Maker, boasting 550W power, is your ticket to effortless baking bliss. With features like a 13-hour delay start and a 1-hour quick bake, enjoy fresh loaves tailored to your schedule. From customisable crusts to versatile settings, elevate your baking experience and savour the taste of perfection!', 1999)
 
-let item3 = new CreateItem(3,'Vans Filmore Hi Black/White Sneakers', 'Shoes', 'https://thefoschini.vtexassets.com/arquivos/ids/150992133-800-1067?v=638485462160430000&width=800&height=1067&aspect=true','', 'he Filmore shoe is a skate-influenced design with double-stitched multi-paneled upper for durability, with a padded tongue & collar. It has a vulcanized construction and the original waffle outsole', 1199.95)
+let item3 = new CreateItem(3,'Vans Filmore Hi Black/White Sneakers', 'Shoes', 'https://thefoschini.vtexassets.com/arquivos/ids/150992133-800-1067?v=638485462160430000&width=800&height=1067&aspect=true',1, 'he Filmore shoe is a skate-influenced design with double-stitched multi-paneled upper for durability, with a padded tongue & collar. It has a vulcanized construction and the original waffle outsole', 1199.95)
 
 let item4 = new CreateItem(4,'Older Boys Grey Melton Coat', 'Clothing', 'https://thefoschini.vtexassets.com/arquivos/ids/152171694-800-1067?v=638502382297800000&width=800&height=1067&aspect=true',1, 'The perfect layering piece in his wardrobe this season. Pair this coat with his fave jeans and sweater for a stylish outfit.',369.99)
 
-let item5 = new CreateItem(5, 'Suit Bottoms', 'Formal Wear', 'https://sihlandlo-schillo.github.io/HOSTEDIMAGES/images/FormalWearBottom.webp', '', 'Crafted by the oldest Formal Wear designer in the world, and that is none other Schillo KOTM', 10000.00)
+let item5 = new CreateItem(5, 'Suit Bottoms', 'Formal Wear', 'https://sihlandlo-schillo.github.io/HOSTEDIMAGES/images/FormalWearBottom.webp',1, 'Crafted by the oldest Formal Wear designer in the world, and that is none other Schillo KOTM', 10000.00)
 
-let item6 = new CreateItem(6,'ROLEX Oyster Perpetual 31', 'Accessories', 'https://asset.topwatch.co.za/ZA/stream/66696c5f3136373031385f323032342d30312d32332031323a32313a3034?filename=watch-club-rolex-oyster-perpetual-latest-release-green-dial-ref-277200-year-2020-14187-wb.jpgwbwbwbwb11.jpg&country_code=ZA','', 'Pre-owned, Unworn. This Timepiece has undergone a thorough inspection of water resistance, accuracy, functionality and condition to determine the level of reconditioning required to meet our strict standards. It has also been referenced against technical documents and manufacturer records where available to ensure authenticity and a clean history.', 145000)
+let item6 = new CreateItem(6,'ROLEX Oyster Perpetual 31', 'Accessories', 'https://asset.topwatch.co.za/ZA/stream/66696c5f3136373031385f323032342d30312d32332031323a32313a3034?filename=watch-club-rolex-oyster-perpetual-latest-release-green-dial-ref-277200-year-2020-14187-wb.jpgwbwbwbwb11.jpg&country_code=ZA',1, 'Pre-owned, Unworn. This Timepiece has undergone a thorough inspection of water resistance, accuracy, functionality and condition to determine the level of reconditioning required to meet our strict standards. It has also been referenced against technical documents and manufacturer records where available to ensure authenticity and a clean history.', 145000)
 
 let items =[item1, item2, item3, item4, item5]
 
@@ -36,11 +36,11 @@ function myProds() {
         <div class="card-body">
           <h5 class="card-title">${item.name}</h5>
           <p class="card-text">R${item.price}</p>
-          <button type="button" class="btn btn-secondary" id="view-More" data-bs-toggle="modal" data-bs-target="#exampleModal">View More</button>
+          <button type="button" class="btn btn-secondary" id="view-More" data-bs-toggle="modal" data-bs-target="#exampleModal${item.id}">View More</button>
           <button type="button" class="btn btn-secondary" id="purchase" value="${item.id}">Add to Cart</button>
       </div>
       <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal${item.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -48,7 +48,8 @@ function myProds() {
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <img src="${item.image}" alt="" id="modalImage" >
+        
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -85,14 +86,6 @@ let purchaseButtons = document.querySelectorAll('#purchase')
  })
 
 //  VIEW MORE BUTTON 
-
-let viewMore = document.querySelector('#View-more')
-function viewing() {
-    
-    viewMore.addEventListener('click', ()=>{
-    
-    })
-}
 
  //SORT BUTTON
 
